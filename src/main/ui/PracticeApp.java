1134dbe00c66711aff57404daa9ce462e23c6d3f
njerem01 <njerem01@students.cs.ubc.ca>
2024-10-08 21:52:07 -0700

    Created PracticeApp UI class, and added specifications for PracticeApp class

---
+++ src/main/ui/PracticeApp.java
@@ -0,0 +1,121 @@
+package ui;
+
+import java.time.Month;
+
+import model.Song;
+
+public class PracticeApp {
+
+    // EFFECTS: creates one instance of Conmoto Music Practice Journal console app
+    public PracticeApp() {
+
+    }
+
+    // EFFECTS: displays and processes inputs for the main menu
+    public void handleMainMenu() {
+
+    }
+
+    // EFFECTS: displays a list of commands that can be used in the main menu
+    public void displayMenu() {
+
+    }
+
+    // EFFECTS: processes the user's input in the main menu
+    public void processMenuCommands(String input) {
+
+    }
+
+    // EFFECTS: displays the title of all songs in the song library
+    public void viewSongLibrary() {
+
+    }
+
+    // EFFECTS: displays a list of commands that can be used in the song library menu
+    public void displayViewMenu() {
+
+    }
+
+    // EFFECTS: processes the user's input in the song library menu
+    public void processViewCommands(String input) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds a song to the song library
+    public void addSong() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: removes a song from the song library
+    public void removeSong() {
+        
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prints the song titles of all songs in the song library that is composed by the given composer
+    public void filterByComposer() {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prints the song titles of all songs in the song library that is composed for the given instrument
+    public void filterByInstrument() {
+
+    }
+
+    // EFFECTS: view details of a specific song, including its title, composer, instrument, bar count, and tempo
+    public void viewSongDetails(Song s) {
+
+    }
+
+    // EFFECTS: displays a list of commands that can be used in the song details menu
+    public void displaySongMenu() {
+
+    }
+
+    // EFFECTS: processes the user's input in the song details menu
+    public void processSongCommands() {
+
+    }
+
+    // EFFECTS: starts a practice session for a given song, and monitors practice duration.
+    public void practice(Song s) {
+        
+    }
+
+    // MODIFIES: this
+    //EFFECTS: ends practice session, and prompts user for practice session details, then logs the practice session
+    public void endPractice(Song s) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: displays the practice duration, tempo, bars practiced, and mastery of all practice sessions in a given month of the year
+    public void viewMonthlyProgress(Song s) {
+
+    }
+
+    // EFFECTS: displays all practice sessions of a given song along with their details (practice duration, tempo, etc.)
+    public void viewPracticeHistory(Song s) {
+
+    }
+
+    // MODIFIES: this
+    // EFFECTS: prints a closing message and marks the program as not running
+    public void quitApp() {
+
+    }
+
+    // EFFECTS: returns the user to the song library menu containing all songs in the library
+    public void returnToSongLibraryMenu() {
+
+    }
+
+    // EFFECTS: returns a Month object that corresponds to the month name string
+    public Month convertStringToMonth(String month) {
+        return null;
+    }
+
+}
