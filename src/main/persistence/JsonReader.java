dfec603dab20c351deaa038968f3e318706e6e51
njerem01 <njerem01@students.cs.ubc.ca>
2024-10-22 13:10:52 -0700

    Added implementation for JsonReader class, fixed bugs in JsonReaderTest

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -10,46 +10,115 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 import org.json.*;
+import org.junit.experimental.categories.Category;
 
 // Represents a reader that reads a song library from JSON data stored in file
 // All code in this class is modelled on the JsonSerializationDemo
 public class JsonReader {
-    
+    private String source;
 
     // EFFECTS: constructs reader to read from source file
     public JsonReader(String source) {
-        
+        this.source = source;
+
     }
 
     // EFFECTS: reads a song library from file and returns it;
     // throws IOException if an error occurs reading data from file
     public SongLibrary read() throws IOException {
-        return null;
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseSongLibrary(jsonObject);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        return null;
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
     // EFFECTS: parses song library from JSON object and returns it
     private SongLibrary parseSongLibrary(JSONObject jsonObject) {
-        return null;  
+        SongLibrary sl = new SongLibrary();
+        addSongs(sl, jsonObject);
+        return sl;
     }
 
     // MODIFIES: sl
     // EFFECTS: parses songs from JSON object and adds them to song library
     private void addSongs(SongLibrary sl, JSONObject jsonObject) {
-        
+        JSONArray jsonArray = jsonObject.getJSONArray("songList");
+        for (Object json : jsonArray) {
+            JSONObject nextSong = (JSONObject) json;
+            addSong(sl, nextSong);
         }
-    
+    }
 
     // MODIFIES: sl
     // EFFECTS: parses one song from JSON object and adds it to song library
     private void addSong(SongLibrary sl, JSONObject jsonObject) {
-        
+        String title = jsonObject.getString("title");
+        String composer = jsonObject.getString("composer");
+        String instrument = jsonObject.getString("instrument");
+        int barCount = jsonObject.getInt("barCount");
+        int tempo = jsonObject.getInt("tempo");
+        List<Session> sessions = getSessionsFromJSON(jsonObject);
+        List<Day> days = getDaysFromJSON(jsonObject);
+        Song song = new Song(title, composer, instrument, barCount, tempo);
+        for (Session s : sessions) {
+            song.logSessionToSessionList(s);
+        }
+        for (Session s : sessions) {
+            song.logSessionToDay(s);
+        }
+        sl.addSong(song);
+    }
+
+    public List<Session> getSessionsFromJSON(JSONObject jsonObject) {
+        List<Session> sessions = new ArrayList<>();
+        JSONArray jsonArray = jsonObject.getJSONArray("sessions");
+        for (Object json : jsonArray) {
+            JSONObject nextSession = (JSONObject) json;
+            int tempo = nextSession.getInt("tempo");
+            int startBar = nextSession.getInt("startBar");
+            int endBar = nextSession.getInt("endBar");
+            float mastery = nextSession.getFloat("mastery");
+            long duration = nextSession.getLong("duration");
+            LocalDate date = LocalDate.parse(nextSession.getString("date"));
+            LocalTime startTime = LocalTime.parse(nextSession.getString("startTime"));
+            LocalTime endTime = LocalTime.parse(nextSession.getString("endTime"));
+            Session newSession = new Session(tempo, startBar, endBar, mastery, duration, date, startTime, endTime);
+            sessions.add(newSession);
+        }
+        return sessions;
+    }
+
+    public List<Day> getDaysFromJSON(JSONObject jsonObject) {
+        List<Day> days = new ArrayList<>();
+        JSONArray jsonArray = jsonObject.getJSONArray("days");
+        for (Object json : jsonArray) {
+
+            JSONObject nextDay = (JSONObject) json;
+            List<Session> sessions = getSessionsFromJSON(nextDay);
+            LocalDate date = LocalDate.parse(nextDay.getString("date"));
+            Day newDay = new Day(date);
+            for (Session s : sessions) {
+                newDay.addSession(s);
+            }
+            days.add(newDay);
+        }
+
+        return days;
     }
 }
-
--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -48,17 +48,17 @@ public class JsonReaderTest extends JsonTest {
             List<Song> songs = sl.getSongList();
             assertEquals(2, songs.size());
 
-            Session s1 = new Session(12, 11, 50, 3.3f, 120, LocalDate.of(2024, 12, 9), LocalTime.of(22, 35, 45, 431239000), LocalTime.of(23, 1, 7, 123124000));
-            Session s2 = new Session(69, 30, 100, 5, 900, LocalDate.of(2020, 2, 11), LocalTime.of(12, 22, 33, 000333000), LocalTime.of(14, 12, 54, 434544000));
-            Session s3 = new Session(70, 31, 101, 6.6f, 100, LocalDate.of(2020, 2, 11), LocalTime.of(15, 22, 33, 000333000), LocalTime.of(16, 12, 54, 434544000));
+            Session s1 = new Session(12, 11, 50, 3.3f, 120, LocalDate.of(2024, 12, 9), LocalTime.of(22, 33, 45, 431239000), LocalTime.of(23, 1, 7, 123124000));
+            Session s2 = new Session(69, 30, 100, 5, 900, LocalDate.of(2020, 2, 11), LocalTime.of(12, 22, 33, 333000), LocalTime.of(14, 12, 54, 434544000));
+            Session s3 = new Session(70, 31, 101, 6.6f, 100, LocalDate.of(2020, 2, 11), LocalTime.of(15, 22, 33, 333000), LocalTime.of(16, 12, 54, 434544000));
             Session s4 = new Session(120, 1, 30, 7, 1009, LocalDate.of(1999, 10, 22), LocalTime.of(12, 33, 47, 900922000), LocalTime.of(17, 11, 37, 555444000));
-            Session s5 = new Session(100, 2, 100, 1.1f, 550, LocalDate.of(1456, 4, 30), LocalTime.of(4, 324, 30, 234111000), LocalTime.of(7, 22, 34, 908456000));
+            Session s5 = new Session(100, 2, 100, 1.1f, 550, LocalDate.of(1456, 4, 30), LocalTime.of(4, 24, 30, 234111000), LocalTime.of(7, 22, 34, 908456000));
             Day d1 = new Day(LocalDate.of(2024, 12, 9));
             Day d2 = new Day(LocalDate.of(2020, 2, 11));
             Day d3 = new Day(LocalDate.of(1999, 10, 22));
             Day d4 = new Day(LocalDate.of(1456, 4, 30));
             d1.addSession(s1);
-            d1.addSession(s2);
+            d2.addSession(s2);
             d2.addSession(s3);
             d3.addSession(s4);
             d4.addSession(s5);
